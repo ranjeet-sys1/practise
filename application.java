ff682af26d65b187efe01120bbbb11db848ec903
Ranjeet <ranjeetsuman123@gmail.com>
2020-07-14 19:09:48 +0530

    fouth edit

--- application.java
+++ application.java
@@ -1,5 +1,5 @@
 public class Application{
 	public static void main(String args[]){
-	System.out.println("GITHUB Practice");
+	System.out.println("************^^^^^####GITHUB Practice------************^^^^^####");
       }
 }
